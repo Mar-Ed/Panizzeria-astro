e15e93ed983245b03acbd2798af22025310c3479
Mar-Ed <158010261+Mar-Ed@users.noreply.github.com>
2025-06-07 01:12:02 -0500

    fix: fix scroll bar sizes

--- src/components/react/Scrollbar.jsx
+++ src/components/react/Scrollbar.jsx
@@ -1,40 +1,61 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 
-import '../../css/styleSwiper.css';
-
+import "../../css/styleSwiper.css";
+import "../../css/base.css";
 // import required modules
-import { FreeMode, Pagination } from 'swiper/modules';
+import { FreeMode, Pagination } from "swiper/modules";
 
 export default function App() {
   return (
     <>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide><img src='/img/platos/Americana.jpg' class="slider-plato"></img></SwiperSlide>
-        <SwiperSlide><img src='/img/platos/Burger.jpg' class="slider-plato"></img></SwiperSlide>
-        <SwiperSlide><img src='/img/platos/Campesina.jpg' class="slider-plato"></img></SwiperSlide>
-        <SwiperSlide><img src='/img/platos/Suprema.jpg' class="slider-plato"></img></SwiperSlide>
-        <SwiperSlide><img src='/img/platos/Pepperoni.jpg' class="slider-plato"></img></SwiperSlide>
-        <SwiperSlide><img src='/img/platos/Cuatro-quesos.jpg' class="slider-plato"></img></SwiperSlide>
-        <SwiperSlide><img src='/img/platos/Especial.jpg' class="slider-plato"></img></SwiperSlide>
-        <SwiperSlide><img src='/img/platos/Fourseasons.jpg' class="slider-plato"></img></SwiperSlide>
-        <SwiperSlide><img src='/img/platos/Hawaiana.jpg' class="slider-plato"></img></SwiperSlide>
-      </Swiper>
+      <div className="wrapper">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper"
+          class="wrapper"
+        >
+          <SwiperSlide>
+            <img src="/img/platos/Americana.jpg" class="slider-plato"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/img/platos/Burger.jpg" class="slider-plato"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/img/platos/Campesina.jpg" class="slider-plato"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/img/platos/Suprema.jpg" class="slider-plato"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/img/platos/Pepperoni.jpg" class="slider-plato"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/img/platos/Cuatro-quesos.jpg" class="slider-plato"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/img/platos/Especial.jpg" class="slider-plato"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/img/platos/Fourseasons.jpg" class="slider-plato"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src="/img/platos/Hawaiana.jpg" class="slider-plato"></img>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </>
   );
 }
