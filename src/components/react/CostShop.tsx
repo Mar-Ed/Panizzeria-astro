27cd550d2599522ef0b0fedbca66a33659f6e4ec
unknown <adr14nm4_30_12@hotmail.com>
2025-06-10 15:35:40 -0500

    refactor: edit the side bar

---
+++ src/components/react/CostShop.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { useState } from "react";
+import CartShopping from "../svg/CartShopping.astro";
+
+type CartShopProps = {
+    costShop: string;
+  };
+export default function CartShop({costShop}:CartShopProps) {
+    const [cost,SetCost]=useState<string>(costShop)
+    return(
+        <>
+            <span>S/</span><span>{cost}</span>
+        </>
+    )
+}
