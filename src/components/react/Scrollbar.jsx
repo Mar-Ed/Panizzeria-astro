6ae9051bcafec5673fa4a9ed21e272af5847b653
Mar-Ed <158010261+Mar-Ed@users.noreply.github.com>
2025-06-19 00:40:45 -0500

    refactor: changed css styles footer component, hero and us

--- src/components/react/Scrollbar.jsx
+++ src/components/react/Scrollbar.jsx
@@ -16,6 +16,19 @@ export default function App() {
   return (
     <>
       <div className="wrapper">
+        <div className="title-container">
+          <h1 className="title">
+            <span className="block">Descubre</span>
+            <span className="block">Nuestros Platos</span>
+          </h1>
+          <div className="text-container">
+            <p>
+              Selecciona los sabores más deliciosos y mejor preparados. Hemos
+              recopilado algunas recetas populares de todo el mundo para que
+              elijas tu favorita.
+            </p>
+          </div>
+        </div>
         <Swiper
           slidesPerView={3}
           spaceBetween={30}
@@ -28,31 +41,79 @@ export default function App() {
           class="wrapper"
         >
           <SwiperSlide>
-            <img src="/img/platos/Americana.png" className="slider-plato"></img>
+            <div className="slider-container">
+              <img
+                src="/img/platos/Americana.png"
+                className="slider-plato"
+              ></img>
+              <h2 className="slider-title">Americana</h2>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/img/platos/Burger.png" className="slider-plato"></img>
+            <div className="slider-container">
+              <img src="/img/platos/Burger.png" className="slider-plato"></img>
+              <h2 className="slider-title">Pizza Burger</h2>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/img/platos/Campesina.png" className="slider-plato"></img>
+            <div className="slider-container">
+              <img
+                src="/img/platos/Campesina.png"
+                className="slider-plato"
+              ></img>
+              <h2 className="slider-title">Pizza Campesina</h2>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/img/platos/Suprema.png" className="slider-plato"></img>
+            <div className="slider-container">
+              <img src="/img/platos/Suprema.png" className="slider-plato"></img>
+              <h2 className="slider-title">Pizza Suprema</h2>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/img/platos/Pepperoni.png" className="slider-plato"></img>
+            <div className="slider-container">
+              <img
+                src="/img/platos/Pepperoni.png"
+                className="slider-plato"
+              ></img>
+              <h2 className="slider-title">Pizza Pepperoni</h2>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/img/platos/Cuatro-quesos.png" className="slider-plato"></img>
+            <div className="slider-container">
+              <img
+                src="/img/platos/Cuatro-quesos.png"
+                className="slider-plato"
+              ></img>
+              <h2 className="slider-title">Pizza Cuatro Quesos</h2>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/img/platos/Especial.png" className="slider-plato"></img>
+            <div className="slider-container">
+              <img
+                src="/img/platos/Especial.png"
+                className="slider-plato"
+              ></img>
+              <h2 className="slider-title">Pizza Especial</h2>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/img/platos/Fourseasons.png" className="slider-plato"></img>
+            <div className="slider-container">
+              <img
+                src="/img/platos/Fourseasons.png"
+                className="slider-plato"
+              ></img>
+              <h2 className="slider-title">Pizza Fourseasons</h2>
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src="/img/platos/Hawaiana.png" className="slider-plato"></img>
+            <div className="slider-container">
+              <img
+                src="/img/platos/Hawaiana.png"
+                className="slider-plato"
+              ></img>
+              <h2 className="slider-title">Pizza Hawaiana</h2>
+            </div>
           </SwiperSlide>
         </Swiper>
       </div>
