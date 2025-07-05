6885b4fa98ce6fa92802b225a5c494adfe238e4b
sdmenos1 <adr14nm4_30_12@hotmail.com>
2025-07-05 00:42:39 -0500

    fix: resolve problems

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import "./login.css"; // Asegúrate de que la ruta sea correcta
+import "./login.css";
+
 export default function LoginForm() {
   const [correo, setCorreo] = useState("");
   const [contraseña, setContraseña] = useState("");
   const [isSignUpActive, setIsSignUpActive] = useState(false);
+
   const [form, setForm] = useState({
     nombre: "",
     correo: "",
@@ -11,12 +13,12 @@ export default function LoginForm() {
     rol: "ROLE_ADMIN",
   });
 
-  //Register
+  // Registrar
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleSubmitRegister=async (e:React.FormEvent)=>{
+  const handleSubmitRegister = async (e: React.FormEvent) => {
     e.preventDefault();
     const res = await fetch("http://localhost:8080/api/auth/register", {
       method: "POST",
@@ -30,7 +32,7 @@ export default function LoginForm() {
     } else {
       alert("Error al registrar");
     }
-  }
+  };
 
   const handleSubmitLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -39,7 +41,7 @@ export default function LoginForm() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ correo, contraseña }),
     });
-    
+
     if (res.ok) {
       const data = await res.json();
       localStorage.setItem("token", data.token);
@@ -52,51 +54,87 @@ export default function LoginForm() {
   return (
     <>
       <div className={`container ${isSignUpActive ? "right-panel-active" : ""}`} id="container">
-	<div className="form-container sign-up-container">
-		<form onSubmit={handleSubmitRegister}>
-			<h1>Create Account</h1>
-			<div className="social-container">
-				<a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-				<a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-				<a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-			</div>
-			<span>or use your email for registration</span>
-			<input type="text" placeholder="Nombre" onChange={handleChange} value={form.nombre}/>
-			<input type="email" placeholder="Email" onChange={handleChange} value={form.correo}/>
-			<input type="password" placeholder="Password" onChange={handleChange} value={form.contraseña} />
-			<button>Sign Up</button>
-		</form>
-	</div>
-	<div className="form-container sign-in-container">
-		<form onSubmit={handleSubmitLogin}>
-			<h1>Sign in</h1>
-			<div className="social-container">
-				<a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-				<a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-				<a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-			</div>
-			<span>or use your account</span>
-			<input type="email" placeholder="Email" value={correo}/>
-			<input type="password" placeholder="Password" value={contraseña}/>
-			<a href="#">Forgot your password?</a>
-			<button>Sign In</button>
-		</form>
-	</div>
-	<div className="overlay-container">
-		<div className="overlay">
-			<div className="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
-				<button className="ghost" onClick={() => setIsSignUpActive(false)} id="signIn">Sign In</button>
-			</div>
-			<div className="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-				<button className="ghost" onClick={() => setIsSignUpActive(true)}id="signUp">Sign Up</button>
-			</div>
-		</div>
-	</div>
-</div>
+        <div className="form-container sign-up-container">
+          <form onSubmit={handleSubmitRegister}>
+            <h1>Create Account</h1>
+            <div className="social-container">
+              <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+              <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+              <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+            </div>
+            <span>or use your email for registration</span>
+
+            <input
+              type="text"
+              name="nombre"
+              value={form.nombre}
+              onChange={handleChange}
+              placeholder="Nombre"
+            />
+            <input
+              type="email"
+              name="correo"
+              value={form.correo}
+              onChange={handleChange}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              name="contraseña"
+              value={form.contraseña}
+              onChange={handleChange}
+              placeholder="Password"
+            />
+
+            <button>Sign Up</button>
+          </form>
+        </div>
+
+        <div className="form-container sign-in-container">
+          <form onSubmit={handleSubmitLogin}>
+            <h1>Sign in</h1>
+            <div className="social-container">
+              <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+              <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+              <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+            </div>
+            <span>or use your account</span>
+
+            <input
+              type="email"
+              name="correo"
+              placeholder="Email"
+              value={correo}
+              onChange={(e) => setCorreo(e.target.value)}
+            />
+            <input
+              type="password"
+              name="contraseña"
+              placeholder="Password"
+              value={contraseña}
+              onChange={(e) => setContraseña(e.target.value)}
+            />
+
+            <a href="#">Forgot your password?</a>
+            <button>Sign In</button>
+          </form>
+        </div>
+
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>To keep connected with us please login with your personal info</p>
+              <button className="ghost" onClick={() => setIsSignUpActive(false)} id="signIn">Sign In</button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, Friend!</h1>
+              <p>Enter your personal details and start journey with us</p>
+              <button className="ghost" onClick={() => setIsSignUpActive(true)} id="signUp">Sign Up</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
